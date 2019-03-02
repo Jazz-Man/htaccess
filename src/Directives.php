acaad1c2dd60405c6570a385a027a6d222795fc0
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-02 21:24:49 +0200

    init Directives class

--- src/Directives.php
+++ src/Directives.php
@@ -1,11 +1,32 @@
 <?php
 
-
 namespace JazzMan\Htaccess;
 
+use JazzMan\Htaccess\Constant\AutoloadInterface;
 
-class Directives
+/**
+ * Class Directives.
+ */
+class Directives implements AutoloadInterface
 {
+    /**
+     * @var array
+     */
+    private $data;
 
+    /**
+     * @return mixed|void
+     */
+    public function load()
+    {
+        // TODO: Implement load() method.
+    }
 
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
 }
