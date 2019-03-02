073296f0f1bdde63406e407c5588f102e71bd2b5
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-02 16:53:59 +0200

    added Headers class

---
+++ src/Headers.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace JazzMan\Htaccess;
+
+
+use JazzMan\Htaccess\Constant\AutoloadInterface;
+
+class Headers implements AutoloadInterface
+{
+
+    /**
+     * @return mixed
+     */
+    public function load()
+    {
+        // TODO: Implement load() method.
+    }
+}
