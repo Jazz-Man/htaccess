d26013a3719646094f4a89de1e147aa8c218b4c3
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-02 21:25:29 +0200

    init Deflate

--- src/Performance/Deflate.php
+++ src/Performance/Deflate.php
@@ -6,6 +6,11 @@ use JazzMan\Htaccess\Constant\AutoloadInterface;
 
 class Deflate implements AutoloadInterface
 {
+
+    /**
+     * @var array
+     */
+    private $data;
     /**
      * @return mixed
      */
@@ -13,4 +18,14 @@ class Deflate implements AutoloadInterface
     {
         // TODO: Implement load() method.
     }
+
+
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
 }
