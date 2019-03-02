059fa14b69665c92c5460f4381b136652bc90d74
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-02 16:54:10 +0200

    added Deflate class

---
+++ src/Performance/Deflate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace JazzMan\Htaccess\Performance;
+
+use JazzMan\Htaccess\Constant\AutoloadInterface;
+
+class Deflate implements AutoloadInterface
+{
+    /**
+     * @return mixed
+     */
+    public function load()
+    {
+        // TODO: Implement load() method.
+    }
+}
