7461eef7a0cf5e52e60061f5ed279e0a03e835b7
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-02 16:52:48 +0200

    added AutoloadInterface Interface

---
+++ src/Constant/AutoloadInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace JazzMan\Htaccess\Constant;
+
+/**
+ * Interface AutoloadInterface.
+ */
+interface AutoloadInterface
+{
+
+    /**
+     * @return mixed
+     */
+    public function load();
+}
