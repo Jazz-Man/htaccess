c493fbb58374275e6f5255d2fa79faabc01c59ed
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-04-14 18:35:35 +0300

    init base App class

--- src/App.php
+++ src/App.php
@@ -3,16 +3,16 @@
 namespace JazzMan\Htaccess;
 
 use JazzMan\Htaccess\Constant\AutoloadInterface;
+use JazzMan\Htaccess\Security\ContentSecurityPolicy;
+use JazzMan\Htaccess\Security\Firewall;
 use Tivie\HtaccessParser\HtaccessContainer;
+use Tivie\HtaccessParser\Token\Comment;
 
 /**
- * Class App
- *
- * @package JazzMan\Htaccess
+ * Class App.
  */
 class App
 {
-
     /**
      * @var array
      */
@@ -21,7 +21,7 @@ class App
     /**
      * @var array
      */
-    private $container = [];
+    private $container = [[]];
 
     /**
      * App constructor.
@@ -29,41 +29,59 @@ class App
     public function __construct()
     {
         $this->class_autoload = [
-            Directives::class,
-            Headers::class
+//            Directives::class,
+            Firewall::class,
+//            Headers::class,
+//            ContentSecurityPolicy::class
         ];
 
         $this->loadDependencies();
 
         $this->buildHtaccess();
-
     }
 
     private function loadDependencies()
     {
-        foreach ($this->class_autoload as $item){
+        foreach ($this->class_autoload as $item) {
             try {
                 $class = new \ReflectionClass($item);
-                if ($class->implementsInterface(AutoloadInterface::class)){
+                if ($class->implementsInterface(AutoloadInterface::class)) {
                     /** @var AutoloadInterface $instance */
                     $instance = $class->newInstance();
                     $instance->load();
 
-                    $this->container[] = $instance->getData();
+                    $data = $instance->getData();
+
+                    $this->container[] = \is_array($data) ? $data : [$data];
                 }
             } catch (\ReflectionException $e) {
             }
         }
     }
 
+    /**
+     * @param string $text
+     *
+     * @return \Tivie\HtaccessParser\Token\Comment
+     *
+     * @throws \Tivie\HtaccessParser\Exception\InvalidArgumentException
+     */
+    public static function addComments($text)
+    {
+        $comment = new Comment();
+        $comment->setText((string) $text);
+
+        return $comment;
+    }
+
     private function buildHtaccess()
     {
-        $this->container = array_filter($this->container);
+//        $this->container = array_filter($this->container);
 
-        if (!empty($this->container)){
-
+        if (!empty($this->container)) {
+            $this->container = array_merge(...$this->container);
             $htaccess = new HtaccessContainer($this->container);
-            dump((string)$htaccess);
+            dump((string) $htaccess);
         }
     }
 }
