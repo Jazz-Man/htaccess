86d78c7a2d3ab7ec2ed20fa995d12d4a25be6035
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-04-15 22:41:09 +0300

    added WordPress Multisite support

--- src/WordPress.php
+++ src/WordPress.php
@@ -2,10 +2,37 @@
 
 namespace JazzMan\Htaccess;
 
+use JazzMan\AutoloadInterface\AutoloadInterface;
+use Tivie\HtaccessParser\Exception\Exception;
+
 /**
  * Class WordPress.
  */
-class WordPress extends HtaccessBase
+class WordPress implements AutoloadInterface
 {
     public $config_dir = 'wp';
+
+    public function load()
+    {
+        $dir = WP_HTACCESS_CONFIG_DIR.'/wp';
+
+        $parser = app_htaccess_parser();
+
+        $file = 'base.conf';
+
+        if (is_multisite()) {
+            if (\defined('SUBDOMAIN_INSTALL') && SUBDOMAIN_INSTALL) {
+                $file = 'multisite-subdomain.conf';
+            } else {
+                $file = 'multisite-subfolder.conf';
+            }
+        }
+
+        $file = new \SplFileObject("{$dir}/{$file}");
+
+        try {
+            app()->setContainer($parser->parse($file->openFile()));
+        } catch (Exception $e) {
+        }
+    }
 }
